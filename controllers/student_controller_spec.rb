6448efc012ca07803d24ce7e6cfa5e05e3e64d6d
Raksha <rramteke@deqode.com>
2021-10-20 20:16:53 +0530

    Add controller and request specs

---
+++ controllers/student_controller_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe StudentController, type: :controller do
+
+  describe '#index' do
+    context 'HTML' do
+      let!(:students) { create(:student) }
+
+      it 'render to the index' do
+        get :index
+
+        expect(response).to be_successful
+        expect(response).to render_template :index
+        expect(assigns(:students)).to match_array(students)
+      end
+    end
+  end
+
+  describe '#new' do
+    context 'HTML' do
+      it 'render to the new' do
+        get :new
+
+        expect(response).to render_template :new
+        expect(assigns(:student)).to be_a_new Student
+      end
+    end
+  end
+
+  describe '#create' do
+    context 'HTML' do
+      context 'valid' do
+        it 'redirects to index' do
+
+          post :create, params: { student: attributes_for(:student) }
+
+          expect(response).to redirect_to student_index_path
+          expect(assigns(:student)).to be_a Student
+          expect(assigns(:student).persisted?).to eq true
+        end
+      end
+      context 'invalid' do
+        it 'render to the new' do
+
+          get :new, params: { student: attributes_for(:student, :invalid) }
+
+          expect(response).to render_template :new
+          expect(assigns(:student)).to be_a Student
+          expect(assigns(:student).persisted?).to eq false
+        end
+      end
+    end
+  end
+end
